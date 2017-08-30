e0d96ae5c87c4593013297d3f835061e50fdbd66
Harry Andriyan <harry.andriyan@gmail.com>
2017-08-30 11:13:49 +0700

    add antara project

---
+++ antara/antara/spiders/antara_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.selector import Selector
+from antara.items import AntaraItem
+
+
+class AntaraSpider(scrapy.Spider):
+    name = "antara"
+    allowed_domains = ["antaranews.com"]
+    start_urls = [
+        "http://www.antaranews.com/terkini",
+    ]
+
+    def parse(self, response):
+        """ This function parses a property page.
+
+        @url http://www.antaranews.com/terkini
+        @returns items
+        """
+
+        indeks = Selector(response).xpath('//ul[@class="ul_rubrik"]/li')
+
+        for indek in indeks:
+            item = AntaraItem()
+            item['title'] = indek.xpath('div/div[@class="bxpd"]/h3/a/text()').extract()[0]
+            item['link'] = "http://www.antaranews.com" + indek.xpath('div/div[@class="bxpd"]/h3/a/@href').extract()[0]
+            item['images'] = indek.xpath('div/div[@class="imgpg"]/a/img/@src').extract()[0]
+            item['category'] = ""
+            item['date'] = ""
+            item['desc'] = ""
+
+            yield item
