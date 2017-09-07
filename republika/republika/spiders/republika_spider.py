d1b805b2c750274f8f57317af6f69d8d8053631c
Harry Andriyan Maulana <harry.andriyan@sebangsa.com>
2017-09-07 14:17:34 +0700

    add parse detail function

--- republika/republika/spiders/republika_spider.py
+++ republika/republika/spiders/republika_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import sys
 from scrapy.selector import Selector
+from scrapy.http.request import Request
 from republika.items import RepublikaItem
 
 
@@ -10,25 +11,27 @@ class RepublikaSpider(scrapy.Spider):
     start_urls = [
         "http://www.republika.co.id/indeks",
     ]
+    detail_url = []
 
     def parse(self, response):
         """ This function parses a property page.
-
         @url http://www.republika.co.id/indeks
         @returns items
         """
-
+        print "Crawling list of news"
         indeks = Selector(response).xpath('//div[@class="wp-indeks"]')
         indeks_length = len(indeks)
         if float(indeks_length) > 0:
             for indek in indeks:
                 item = RepublikaItem()
+                news_link = indek.xpath('a/@href').extract()[0]
                 item['title'] = indek.xpath('a/div[@class="item3"]/text()').extract()[0]
-                item['link'] = indek.xpath('a/@href').extract()[0]
+                item['link'] = news_link
+                detail_request = Request(news_link, callback=self.parse_detail)
                 item['images'] = indek.xpath('a/div[@class="item2"]/div[@class="img-ct"]/img/@src').extract()[0]
                 item['category'] = ""
                 item['date'] = indek.xpath('a/div[@class="item1"]/div[@class="date"]/text()').extract()[0]
-                item['desc'] = ""
+                item['desc'] = detail_request
 
                 yield item
         else:
@@ -46,3 +49,10 @@ class RepublikaSpider(scrapy.Spider):
                 response.urljoin(next_page_link),
                 callback=self.parse
             )
+
+    def parse_detail(self, response):
+        print "Crawling detail news"
+        selector = Selector(response)
+        description = selector.xpath('//div[@class="content-detail"]/p/text()').extract()[0]
+
+        return description
